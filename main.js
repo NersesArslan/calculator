c0744f6e486133371d7f0d58d0cf43f82d48a4d2
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-11-21 15:04:51 -0800

    operator keys functional, need to be able to append multiple numbers for second value

--- main.js
+++ main.js
@@ -1,54 +1,64 @@
-const add = (a, b) => {
-    return a + b;
-}
+const calculate = (n1, operator, n2) => {
+  let result = ''
 
-const subtract = (a, b) => {
-    return a - b;
-}
-
-const multiply = (a, b) => {
-    return a * b;
-}
-
-const divide = (a, b) => {
-    return a / b;
-}
-
-const operate = (operator, a, b) => {
-    if(operator === '+'){
-        return add(a, b);
-    } else if(operator === '-'){
-        return subtract(a, b)
-    } else if(operator === '*'){
-        return multiply(a, b);
-    } else if(operator === '/'){
-        return divide(a, b);
-    }
+  if(operator === 'add'){
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if(operator === 'subtract'){
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if(operator === 'multiply'){
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if(operator === 'divide'){
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+  return result
 }
 
 
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".keys");
 
+const display = document.querySelector('.display');
+
 
 keys.addEventListener("click", e => {
+  if(e.target.matches('button')) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
   if(!action) {
-    console.log('number key!');
-  } else if(
+    if(displayedNum === '0' || previousKeyType === 'operator'){
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayedNum + keyContent
+    }
+  } 
+  if(
     action === 'add' ||
     action === 'subtract' ||
     action === "multiply" ||
     action === "divide"
   ){
-    console.log("operator key")
-  } else if(action === "clear"){
-    console.log("clear key")
-  } else if (action === "decimal"){
-    console.log("decimal key")
-  } else if(action === "calculate"){
-    console.log("equal key")
+    key.classList.add('is-depressed')
+    calculator.dataset.previousKeyType = "operator";
+    calculator.dataset.firstValue = displayedNum
+    calculator.dataset.operator = action
   }
-
+  if(action === "clear"){
+    display.textContent = '0'
+  } 
+  if (action === "decimal"){
+    display.textContent = displayedNum + '.';
+  } 
+   if(action === "calculate"){
+    const secondValue = displayedNum;
+    const operator = calculator.dataset.operator
+    const firstValue = calculator.dataset.firstValue
+    
+   display.textContent = calculate(firstValue, operator, secondValue)
+  }
+  }
 })
